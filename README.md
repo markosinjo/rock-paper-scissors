84496d8f59b42bb9ce20cba9e0440f8fb029b617
markosinjo <stojanovic.marko0@outlook.com>
2023-02-27 03:29:06 +0100

    Add score display on the page, and winner announcement

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # rock-paper-scissors
 
-Rock paper scissors game thats played in the console
+Rock paper scissors game thats played with a click of a button 
--- script.js
+++ script.js
@@ -10,63 +10,100 @@ function getComputerChoice () {
 }
 
 function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let result = [];
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            console.log("Draw, rock and rock");
-            return 0;
+            result[0] = "Draw, rock and rock";
+            result[1] = 0;
+            return result;
 
         } else if (computerSelection === "paper") {
-            console.log("You lose, paper beats rock");
-            return -1;
+            result[0] = "You lose, paper beats rock";
+            result[1] = -1;
+            return result;
 
-        } else console.log("You win, rock beats scissors");
-               return 1;
+        } else result[0] = "You win, rock beats scissors";
+                result[1] = 1;
+                return result;
 
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            console.log("You win, paper beats rock");
-            return 1;
+            result[0] = "You win, paper beats rock";
+            result[1] = 1;
+            return result;
 
         } else if (computerSelection === "paper") {
-            console.log("Draw, paper and paper");
-            return 0;
+            result[0] = "Draw, paper and paper";
+            result[1] = 0;
+            return result;
 
-        } else console.log("You lose, scissors beat paper");
-               return -1;
+        } else result[0] = "You lose, scissors beat paper";
+                result[1] = -1;
+                return result;
 
     } else {
         if (computerSelection === "rock") {
-            console.log("You lose, rock beats scissors");
-            return -1;
+            result[0] = "You lose, rock beats scissors";
+            result[1] = -1;
+            return result;
 
         } else if (computerSelection === "paper") {
-             console.log("You win, scissors beat paper");
-             return 1;
+             result[0] = "You win, scissors beat paper";
+             result[1] = 1;
+             return result;
 
         } else { 
-            console.log("Draw, scissors and scissors");
-            return 0;
+            result[0] = "Draw, scissors and scissors";
+            result[1] = 0;
+            return result;
         }
     }
-}
-
-function game() {
-    // Play the game 5 times and show the winner
-    let result = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Chose rock, paper or scissors").toLowerCase();
-        let computerSelection = getComputerChoice();
-        result += playRound(playerSelection, computerSelection);
-    }
-    if (result > 0) {
-        console.log("You win " + result);
-    } else if (result < 0) {
-        console.log("You lose " + result * -1);
-    } else console.log("Its a draw");
 }
 
 
 
 const btns = document.querySelectorAll(".btn");
+const roundDisplay = document.querySelector("#roundResult");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
 
-btns.forEach((button) => {button.addEventListener("click",  (e) => playRound(e.target.innerText, getComputerChoice()))});
+let totalPlayerScore = 0;
+let totalComputerScore = 0;
+
+btns.forEach((button) => {button.addEventListener("click",  (e) => {
+
+    const result = playRound(e.target.innerText, getComputerChoice());
+    // Display round result on page
+    roundDisplay.textContent = result[0];
+
+    if (result[1] === 1) {
+        totalPlayerScore++;
+    }
+    if (result[1] === -1) {
+        totalComputerScore++;
+    }
+    
+    playerScore.textContent = "Player score: " + totalPlayerScore;
+    computerScore.textContent = "Computer score: " +totalComputerScore;
+
+
+    // reset the text style to default 
+    roundDisplay.style.cssText = "color:black; font-size: 48px; font-weight: 400;"
+
+    // Declare the winner, reset score after
+    if (totalPlayerScore === 5) {
+        roundDisplay.textContent = "YOU WIN"
+        roundDisplay.style.cssText = "color: green; font-size: 84px; font-weight: 900;";
+        totalPlayerScore = 0;
+        totalComputerScore = 0;
+    } 
+
+    if (totalComputerScore === 5) {
+        roundDisplay.textContent = "YOU LOSE"
+        roundDisplay.style.cssText = "color: red; font-size: 84px;";
+        totalComputerScore = 0;
+        totalPlayerScore = 0;
+    } 
+})});
+
